f48681f26023081e30ca84314deb08bbd6ffaf00
aannddrree <aannddrree.silva@gmail.com>
2018-10-08 21:41:54 -0300

    implantação

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webApiExample1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
